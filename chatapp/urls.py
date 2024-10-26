9498aa85ba7ec8310c0994396ab0184e48f1d2c6
jagadeesh <iamjagadeesh.d@gmail.com>
2024-10-26 11:33:41 +0530

    new

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('funchat/',views.Myview,name="index"),
+    path('',views.Myview,name="index"),
     path('chatroom/<str:receiver>/',views.chatroom,name="chat_room"),
     path('signup/',views.signup,name='signup'),
     path('signin/',views.signin,name='signin'),
