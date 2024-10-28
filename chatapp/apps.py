1d0e923b8bb9dbf4405b3157551a4fae73434900
jagadeesh <iamjagadeesh.d@gmail.com>
2024-10-28 11:18:28 +0530

    new version

--- chatapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatapp'
-
-    def ready(self):
-        import chatapp.signals
